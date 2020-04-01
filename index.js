cc26fa8cb444c1b97db04ee5c3360d55cf90d3e2
shallisey <47544304+shallisey@users.noreply.github.com>
2020-04-01 09:23:58 -0500

    Use middleware for parsing the body of a form and returning the data when you send a post request.

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+//adds to all routes
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('hi there');
+  res.send(`
+  <div>
+  <form method="POST">
+    <input name="email" placeholder='email' />
+    <input name="password" placeholder='password' />
+    <input name="passwordconfirmation"placeholder='password confirmation' />
+    <button>Sign Up</button
+  </form>
+</div>
+  `);
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  res.send('account created');
 });
 
 app.listen(3000, () => {
