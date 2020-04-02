18ffdabcd335c8b4bdcccac80e6901151b6219ca
shallisey <47544304+shallisey@users.noreply.github.com>
2020-04-02 08:53:21 -0500

    Set up the Users Repository. Declare the filename to be used in all methods. Create getAll method for retrieving all of the user.

---
+++ repositories/users.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+class UsersRepository {
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Creating a repository requires a filename.');
+    }
+
+    this.filename = filename;
+    try {
+      fs.accessSync(this.filename);
+    } catch (err) {
+      fs.writeFileSync(this.filename, '[]');
+    }
+  }
+  async getAll() {
+    //returning the parsed data in a file
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf8'
+      })
+    );
+  }
+}
+const test = async () => {
+  const repo = new UsersRepository('users.json');
+  const users = await repo.getAll();
+  console.log(users);
+};
+
+test();
