673972ddcf0e860e40671839006d84cc476fbc7a
shallisey <47544304+shallisey@users.noreply.github.com>
2020-04-02 09:16:45 -0500

    Create the create and writeAll methods to UserRepository class.

--- repositories/users.js
+++ repositories/users.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const crypto = require('crypto');
 
 class UsersRepository {
   constructor(filename) {
@@ -21,9 +22,24 @@ class UsersRepository {
       })
     );
   }
+  async create(attrs) {
+    const records = await this.getAll();
+    records.push(attrs);
+
+    await this.writeAll(records);
+  }
+  async writeAll(records) {
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {}
 }
 const test = async () => {
   const repo = new UsersRepository('users.json');
+  await repo.create({ email: 'test@test.com', password: 'password' });
   const users = await repo.getAll();
   console.log(users);
 };
