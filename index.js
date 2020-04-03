c2f4fe4b008472d79043054157e3e8670d379e28
shallisey <47544304+shallisey@users.noreply.github.com>
2020-04-03 10:17:36 -0500

    Install crypto from the nodejs api. Create a way to salt and hash passwords for security and validate the salt and hashed password for user signin.

--- index.js
+++ index.js
@@ -1,27 +1,86 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
+const usersRepo = require('./repositories/users');
 
 const app = express();
 
 //adds to all routes
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ['gfaosdlnfoaefbaosifoa09sdf09h123i8h112433yuf56']
+  })
+);
 
-app.get('/', (req, res) => {
+app.get('/signup', (req, res) => {
   res.send(`
   <div>
+  Your id is: ${req.session.userId}
   <form method="POST">
     <input name="email" placeholder='email' />
     <input name="password" placeholder='password' />
-    <input name="passwordconfirmation"placeholder='password confirmation' />
+    <input name="passwordConfirmation"placeholder='password confirmation' />
     <button>Sign Up</button
   </form>
 </div>
   `);
 });
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+app.post('/signup', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body;
+
+  const existingUser = await usersRepo.getOneBy({ email });
+  if (existingUser) {
+    return res.send('Email in use.');
+  }
+  if (password !== passwordConfirmation) {
+    return res.send('passwords must match');
+  }
+  const user = await usersRepo.create({ email, password });
+
+  req.session.userId = user.id;
+
   res.send('account created');
+});
+
+app.get('/signout', (req, res) => {
+  req.session = null;
+  res.send('You are logged out');
+});
+
+app.get('/signin', (req, res) => {
+  res.send(`
+  <div>
+  <form method="POST">
+    <input name="email" placeholder='email' />
+    <input name="password" placeholder='password' />
+    <button>Sign In</button
+  </form>
+</div>
+  `);
+});
+
+app.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await usersRepo.getOneBy({ email });
+
+  if (!user) {
+    return res.send('Email not found.');
+  }
+
+  const validPassword = await usersRepo.comparePasswords(
+    user.password,
+    password
+  );
+  if (!validPassword) {
+    return res.send('invalid password');
+  }
+
+  req.session.userId = user.id;
+
+  res.send('You are signed in.');
 });
 
 app.listen(3000, () => {
--- repositories/users.js
+++ repositories/users.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 const crypto = require('crypto');
+const util = require('util');
+
+const scrypt = util.promisify(crypto.scrypt);
 
 class UsersRepository {
   constructor(filename) {
@@ -25,16 +28,34 @@ class UsersRepository {
   async create(attrs) {
     attrs.id = this.randomId();
 
+    const salt = crypto.randomBytes(16).toString('hex');
+    const buf = await scrypt(attrs.password, salt, 64);
+
     const records = await this.getAll();
-    records.push(attrs);
+    const record = {
+      ...attrs,
+      password: `${buf.toString('hex')}.${salt}`
+    };
+    records.push(record);
 
     await this.writeAll(records);
+    return record;
   }
   async writeAll(records) {
     await fs.promises.writeFile(
       this.filename,
       JSON.stringify(records, null, 2)
     );
+  }
+
+  async comparePasswords(saved, supplied) {
+    // Saved --> passwords saved in our database. 'hashed.salt'
+    //Supplied --> password given to us by a user trying to sign in.
+
+    const [hashed, salt] = saved.split('.');
+    const hashedSuppliedBuf = await scrypt(supplied, salt, 64);
+
+    return hashed === hashedSuppliedBuf.toString('hex');
   }
 
   randomId() {
@@ -78,11 +99,5 @@ class UsersRepository {
     }
   }
 }
-// const test = async () => {
-//   const repo = new UsersRepository('users.json');
 
-//   const user = await repo.getOneBy();
-//   console.log(user);
-// };
-
-// test();
+module.exports = new UsersRepository('users.json');
